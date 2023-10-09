b75eb6ee460927110db764d8d41036c949db4a0f
N-ishant <nishantupreti30@gmail.com>
2023-10-09 15:28:52 +0530

    filtering both the name of Item and Description

--- dom4.js
+++ dom4.js
@@ -8,41 +8,42 @@ form.addEventListener('submit' , addItem);
 //Add item
 function addItem(e){
     e.preventDefault();
-    //console.log(1);
 
-    //Get input Value
-    let newItem = document.getElementById('item').value;
+     // Get input value for item name
+     let newItemName = document.getElementById('item').value;
+     let newItemDescription = document.getElementById('item-description').value;
 
-    // Create new li element
-    let li = document.createElement('li');
-    // Add class
-    li.className = 'list-group-item';
-    // Add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+     //Create new li element
+     let li = document.createElement('li');
+     //Add Class
+     li.className = 'list-group-item';
+     //Add Text Node with input value
+     li.appendChild(document.createTextNode(newItemName + ' ' + newItemDescription));
 
-    // Create delete button elelment
-    let deleteBtn = document.createElement('button');
-    // Add classes to delete button
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    // Append text node
-    deleteBtn.appendChild(document.createTextNode('X'));
+     //Create Delete Button Element
+     let deleteBtn = document.createElement('button');
+     //Add Classes to Delete Button
+     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+     //Append text Node
+     deleteBtn.appendChild(document.createTextNode('X'));
 
-    //Create Edit Button
-    let editBtn = document.createElement('button');
-    //Add Classes to Edit button
-    editBtn.className = 'btn btn-primary btn-sm float-right edit';
-    //Append text node
-    editBtn.appendChild(document.createTextNode('Edit'));
+     //Create Edit button
+     let editBtn = document.createElement('button');
+     //Add Classess to Edit Button
+     editBtn.className = 'btn btn-primary btn-sm float-right edit';
+     //Append text Node
+     editBtn.appendChild(document.createTextNode('Edit'));
 
-    // Append Delete and Edit buttons to li
-    li.appendChild(deleteBtn);
-    li.appendChild(editBtn);
+     //Append Delete and Edit Button's to li
+     li.appendChild(deleteBtn);
+     li.appendChild(editBtn);
 
-    //Append li to itemList(ul)
-    itemList.appendChild(li);
+     //Append li to Item list (ul)
+     itemList.appendChild(li);
 
-    // Clear input fields after adding an item
-    document.getElementById('item').value = '';
+     // Clear input fields after adding an item
+     document.getElementById('item').value = '';
+     document.getElementById('item-description').value = '';
 }
 
 // Delete event
@@ -75,23 +76,23 @@ function editButton(e){
 filter.addEventListener('keyup' , filterItems);
 
 // Filter Items
-function filterItems(e){
-    // Convert Text to lowerCase
+function filterItems(e) {
+    // Convert text to lowercase
     let text = e.target.value.toLowerCase();
-    //console.log(text);
+  
+    // Get List
+    let items = itemList.getElementsByTagName('li');  // array of node
+  
+    // Convert to array
+    Array.from(items).forEach(function (item) {
 
-    //Get List
-    let items = itemList.getElementsByTagName('li');
-    //console.log(items);
-
-    // Convert "HTML Collection" into an ARRAY
-    Array.from(items).forEach(function(item){
         let itemName = item.firstChild.textContent;
-        //console.log(itemName);
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        let itemDescription = item.childNodes[1].textContent;
+  
+        if (itemName.toLowerCase().indexOf(text) != -1 || itemDescription.toLowerCase().indexOf(text) != -1) {
             item.style.display = 'block';
-        }else{
+        } else {
             item.style.display = 'none';
         }
     });
-}
+  }
